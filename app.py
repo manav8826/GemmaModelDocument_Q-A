987d921ecdc26be9d73d8a78bfe1c83fddb7ac26
Manav Gupta <143154389+manav8826@users.noreply.github.com>
2025-11-14 15:49:41 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from langchain_groq import ChatGroq
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
